d875121e212e4597451708e9363541181ac45d74
Matthew Creager AKA Slukehart <matt@symptom6.com>
2013-02-28 09:26:39 -0800

    Minor re-factor numero uno

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,77 +1,87 @@
 /**
- * Without this, contact form NO work
+ * Contact Form
+ * Dependencies: Lodash & jQuery
  */
-$(document).on('ready', function () {
+
+"use strict";
+
+// Create our contact pseudo-model
+function Contact(attributes) {
+    var props, defaults, save;
+
+    defaults = {
+        name    : 'Mr Anonymous',
+        email   : '',
+        message : 'Not "very" talkative'
+    };
+
+    this.props = _.defaults(attributes, defaults);
+}
+
+// Attach a save method to the our contact model
+_.assign(Contact.prototype, {
+    save: function(success, err){
+
+        var request = $.post('contact/submit', this.props);
+            request.fail(function (data) { err($.parseJSON(data.responseText).error) });
+            request.done(success);
+    }
+});
+
+//  
+var contactForm = (function ($) {
     
-    "use strict";
+    // Element container object
+    var el = {};
 
-    function Contact(attributes) {
-        var props, defaults, save;
-
-        defaults = {
-            name    : 'Mr Anonymous',
-            email   : '',
-            message : 'Not "very" talkative'
-        };
-
-        this.props = _.defaults(attributes, defaults);
+    // Public Initializer
+    function init() {
+        _cacheElements();
+        _bindEvents();
     }
 
-    _.assign(Contact.prototype, {
-        save: function(success, err){
+    // Cache elements - so we dip into the DOM less
+    function _cacheElements() {
+        el.form         = $('.contact-form form');
+        el.email_input  = el.form.find('#input-email');
+        el.email_error  = el.email_input.siblings('.error-message');
+    }
 
-            var request = $.post('contact/submit', this.props);
-                request.fail(function (data) { err($.parseJSON(data.responseText).error) });
-                request.done(success);
-        }
-    });
+    // Bind to our form events
+    function _bindEvents() {
+        el.form.on('submit', _formHandler);
+        el.email_input.on('focus', _resetErrors);
+    }
 
-    var contactForm = (function ($) {
-        
-        var el = {};
+    // On contact form submission create a new contact model & save it
+    function _formHandler(ev) {
+        ev.preventDefault();
+        var contact = new Contact(el.form.serializeObject());
+            contact.save(_renderThanks, _renderError);
+    }
 
-        // Public Initializer
-        function init() {
-            _cacheElements();
-            _bindEvents();
-        }
+    // If the contact form was sent to the server successfully we say thank you
+    function _renderThanks() {
+        el.form.hide();
+        $('#thank-you').show();
+    }
 
-        function _cacheElements() {
-            el.form         = $('.contact-form form');
-            el.email_input  = el.form.find('#input-email');
-            el.email_error  = el.email_input.siblings('.error-message');
-        }
+    // Otherwise we display an error
+    function _renderError(message) {
+        el.email_input.addClass('error');
+        el.email_error.text(message);
+    }
 
-        function _bindEvents() {
-            el.form.on('submit', _formHandler);
-            el.email_input.on('focus', _resetErrors);
-        }
+    // When the user focuses on the email input, we get rid of the errors
+    function _resetErrors() {
+        el.email_input.removeClass('error');
+        el.email_error.text('');
+    }
 
-        function _formHandler(ev) {
-            ev.preventDefault();
-            var contact = new Contact(el.form.serializeObject());
-                contact.save(_renderThanks, _renderError);
-        }
+    // Public function returned for initiation
+    return { init: init };
 
-        function _renderThanks() {
-            el.form.hide();
-            $('#thank-you').show();
-        }
+})(jQuery);
 
-        function _renderError(message) {
-            el.email_input.addClass('error');
-            el.email_error.text(message);
-        }
-
-        function _resetErrors() {
-            el.email_input.removeClass('error');
-            el.email_error.text('');
-        }
-
-        return { init: init };
-
-    })(jQuery);
-
-    contactForm.init();
-
-});
+// Instantiate our Contact Form - once the DOM is ready
+$(contactForm.init());
